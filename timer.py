c7ab4f6e1c3a530f5824110aa64961e6eee1bc6a
marcchan <marc.chan89@gmail.com>
2019-02-24 15:29:17 +0800

    added function call optimizations, timer script and optimization result

---
+++ timer.py
@@ -0,0 +1,26 @@
+from timeit import timeit
+import os
+
+if __name__ == "__main__":
+    reps = 3
+    optimized = "nbody_1"
+
+    setup = "from nbody import nbody"
+    pretime = timeit("nbody(100, 'sun', 20000)", setup=setup, number=reps)/reps
+
+    setup = "from {} import nbody".format(optimized)
+    posttime = timeit("nbody(100, 'sun', 20000)", setup=setup, number=reps)/reps
+
+    print("Results of {} vs nbody, average of {} runs:".format(optimized, reps))
+    print("Pre-optimized runtime: {:.2f}s".format(pretime))
+    print("Post-optimized runtime: {:.2f}s".format(posttime))
+    print("Relative speedup: {:.2f}x".format(pretime/posttime))
+
+    with open("{}_results.txt".format(optimized),"w") as f:
+        f.write("Results of {} vs nbody, average of {} runs:".format(optimized, reps))
+        f.write("\n")
+        f.write("Pre-optimized runtime: {:.2f}s".format(pretime))
+        f.write("\n")
+        f.write("Post-optimized runtime: {:.2f}s".format(posttime))
+        f.write("\n")
+        f.write("Relative speedup: {:.2f}x".format(pretime/posttime))
